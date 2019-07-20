f87b51e2380f49aa6b42cbc74a61cc584bdb9be6
nimish <nimish4july1998@gmail.com>
2019-07-20 23:25:17 +0530

    snackbar working fine

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,14 @@ import 'package:pro_logger/widgets/LogEntryCard.dart';
 
 void main() => runApp(MyApp());
 
+class Choice
+{
+	final title,icon;
+	
+	const Choice({this.title, this.icon});
+}
+
+
 class MyApp extends StatelessWidget {
 	// This widget is the root of your application.
 	@override
@@ -30,7 +38,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-	MyHomePage({Key key, this.title}) : super(key: key);
+	final String title;
 	
 	// This widget is the home page of your application. It is stateful, meaning
 	// that it has a State object (defined below) that contains fields that affect
@@ -41,7 +49,7 @@ class MyHomePage extends StatefulWidget {
 	// used by the build method of the State. Fields in a Widget subclass are
 	// always marked "final".
 	
-	final String title;
+	MyHomePage({Key key, this.title}) : super(key: key);
 	
 	@override
 	_MyHomePageState createState() => _MyHomePageState();
@@ -49,8 +57,84 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 	
-	Widget _myListView(BuildContext context) {
+	@override
+	Widget build(BuildContext context) {
+		// This method is rerun every time setState is called, for instance as done
+		// by the _incrementCounter method above.
+		//
+		// The Flutter framework has been optimized to make rerunning build methods
+		// fast, so that you can just rebuild anything that needs updating rather
+		// than having to individually change instances of widgets.
 		
+		
+		const List<Choice> choices = const <Choice>[
+			const Choice(title: 'Car', icon: Icons.directions_car),
+			const Choice(title: 'Bicycle', icon: Icons.directions_bike),
+			const Choice(title: 'Boat', icon: Icons.directions_boat)
+		];
+		
+		return Scaffold(
+			appBar: AppBar(
+				// Here we take the value from the MyHomePage object that was created by
+				// the App.build method, and use it to set our appbar title.
+				title: Text(widget.title),
+				actions: <Widget>[
+					Builder(builder: (context) =>
+						PopupMenuButton<Choice>(
+							onSelected: (choice) => _select(choice, context),
+							itemBuilder: (BuildContext context) {
+								return choices.map((Choice choice) {
+									return PopupMenuItem<Choice>(
+										value: choice,
+										child: Text(choice.title),
+									);
+								}).toList();
+							},
+						),
+					),
+				],
+			),
+			body: Center(
+				// Center is a layout widget. It takes a single child and positions it
+				// in the middle of the parent.
+				child: Column(
+					// Column is also layout widget. It takes a list of children and
+					// arranges them vertically. By default, it sizes itself to fit its
+					// children horizontally, and tries to be as tall as its parent.
+					//
+					// Invoke "debug painting" (press "p" in the console, choose the
+					// "Toggle Debug Paint" action from the Flutter Inspector in Android
+					// Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+					// to see the wireframe for each widget.
+					//
+					// Column has various properties to control how it sizes itself and
+					// how it positions its children. Here we use mainAxisAlignment to
+					// center the children vertically; the main axis here is the vertical
+					// axis because Columns are vertical (the cross axis would be
+					// horizontal).
+					mainAxisAlignment: MainAxisAlignment.center,
+					children: <Widget>[
+						Container(child: _myListView(context), height:  MediaQuery.of(context).size.height-100,)
+					],
+				),
+			),
+		);
+	}
+	
+	
+	void _incrementCounter() {
+		setState(() {
+			// This call to setState tells the Flutter framework that something has
+			// changed in this State, which causes it to rerun the build method below
+			// so that the display can reflect the updated values. If we changed
+			// _counter without calling setState(), then the build method would not be
+			// called again, and so nothing would appear to happen.
+//			_counter++;
+		});
+	}
+	
+	
+	Widget _myListView(BuildContext context) {
 		var logEntry1 = LogEntry(title: "Title", message: "This is a custom message", logLevel: notset);
 		var logEntry2 = LogEntry(title: "Title", message: "This is a custom message", logLevel: debug);
 		var logEntry3 = LogEntry(title: "Title", message: "This is a custom message", logLevel: info);
@@ -74,72 +158,12 @@ class _MyHomePageState extends State<MyHomePage> {
 	}
 	
 	
-	void _incrementCounter() {
-		setState(() {
-			// This call to setState tells the Flutter framework that something has
-			// changed in this State, which causes it to rerun the build method below
-			// so that the display can reflect the updated values. If we changed
-			// _counter without calling setState(), then the build method would not be
-			// called again, and so nothing would appear to happen.
-//			_counter++;
-		});
+	void _select(Choice value, BuildContext context) {
+		print(value.title);
+		Scaffold.of(context).hideCurrentSnackBar();
+		Scaffold.of(context).showSnackBar(new SnackBar(
+			content: new Text(value.title + " selected"),
+		));
 	}
 	
-	
-	@override
-	Widget build(BuildContext context) {
-		// This method is rerun every time setState is called, for instance as done
-		// by the _incrementCounter method above.
-		//
-		// The Flutter framework has been optimized to make rerunning build methods
-		// fast, so that you can just rebuild anything that needs updating rather
-		// than having to individually change instances of widgets.
-		
-		
-		return Scaffold(
-			appBar: AppBar(
-				// Here we take the value from the MyHomePage object that was created by
-				// the App.build method, and use it to set our appbar title.
-				title: Text(widget.title),
-			),
-			body: Center(
-				// Center is a layout widget. It takes a single child and positions it
-				// in the middle of the parent.
-				child: Column(
-					// Column is also layout widget. It takes a list of children and
-					// arranges them vertically. By default, it sizes itself to fit its
-					// children horizontally, and tries to be as tall as its parent.
-					//
-					// Invoke "debug painting" (press "p" in the console, choose the
-					// "Toggle Debug Paint" action from the Flutter Inspector in Android
-					// Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-					// to see the wireframe for each widget.
-					//
-					// Column has various properties to control how it sizes itself and
-					// how it positions its children. Here we use mainAxisAlignment to
-					// center the children vertically; the main axis here is the vertical
-					// axis because Columns are vertical (the cross axis would be
-					// horizontal).
-					mainAxisAlignment: MainAxisAlignment.center,
-					children: <Widget>[
-						Container(child: _myListView(context), height:  MediaQuery.of(context).size.height-100,)
-//								LogEntryCard(logEntry: logEntry1),
-//								LogEntryCard(logEntry: logEntry2),
-//								LogEntryCard(logEntry: logEntry3),
-//								LogEntryCard(logEntry: logEntry4),
-//								LogEntryCard(logEntry: logEntry5),
-//								LogEntryCard(logEntry: logEntry6),
-//								LogEntryCard(logEntry: logEntry7),
-//								LogEntryCard(logEntry: logEntry8),
-//								LogEntryCard(logEntry: logEntry9),
-							],
-						),
-				),
-			floatingActionButton: FloatingActionButton(
-				onPressed: _incrementCounter,
-				tooltip: 'Increment',
-				child: Icon(Icons.add),
-			), // This trailing comma makes auto-formatting nicer for build methods.
-		);
-	}
 }
